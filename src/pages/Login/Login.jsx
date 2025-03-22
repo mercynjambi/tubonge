342da807c42f9c7df7aa96bea4c59495155435cb
MERCY NJAMBI <njambim695@gmail.com>
2025-03-22 16:23:53 +0300

    worked on login logic

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,23 +1,32 @@
-import React from 'react'
+import React , {useState}from 'react'
 import './Login.css'
 import assets from "../../assets/assets"
 
 const Login = () => {
+
+    const[currState, setCurrState] = useState("Sign up")
   return (
     <div className='login'>
         <img src={assets.logo_big} alt="login" className="logo"/>   
         <form className="login-form">
-            <h2>Sign up</h2>
-            <input type="text" placeholder="Username" className='form-input' required/>
+            <h2>{currState}</h2>
+            {currState === "Sign up" ?<input type="text" placeholder="Username" className='form-input' required/> : null}
             <input type="email" placeholder="Email address"  className='form-input' required/>
             <input type="password" placeholder="Password"   className='form-input' required/>
-            <button type="submit" >Sign up</button>
+            <button type="submit" >{currState=== "Sign up" ? "Create account" : "Login now" }</button>
             <div className="login-term">
                 <input type="checkbox" />
                 <p>Agree to the terms and use of policy</p>
             </div>
             <div className='login-forgot'>
-                <p className='login-toggle'> Already have an account ? <span>click here </span></p>
+                {currState === "Sign up" 
+                ?
+                <p className='login-toggle'> Already have an account ? <span onClick={() =>setCurrState("Login")}>Login here </span></p>
+                :
+                <p className='login-toggle'> Create an account ? <span onClick={() =>setCurrState("Sign up")}>click here </span></p>}
+                
+                
+
 
             </div>
             </form>  
