22388d651fc31196a8aa6bebb1a9255429a089c0
MERCY NJAMBI <njambim695@gmail.com>
2025-03-23 23:39:43 +0300

    worked on profile  update functionality

--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, {useState}from 'react'
 import './ProfileUpdate.css'
+import assets from '../../assets/assets';
 
 const ProfileUpdate = () => {
+   const[image, setImage] =useState(false);
   return (
-    <div>ProfileUpdate</div>
+    <div className='profile'>
+      <div className='profile-container'>
+         <form>
+           <h3>Profile Details</h3>
+           <label htmlFor="avatar">
+             <input  onChange ={(e) => setImage(e.target.files[0])} type="file" id="avatar" name="avatar" accept=".png,  .jpg, .jpeg" hidden/>
+             <img src={image? URL.createObjectURL(image) :assets.avatar_icon} alt=""/>
+              upload profile image
+            </label>
+            <input type="text" placeholder='Your name ' required/>
+            <textarea placeholder='Write Profile Bio' required ></textarea>
+            <button type="submit">Save</button>            
+         </form>
+         <img src={ image? URL.createObjectURL(image) :assets.logo_icon} className='profile-pic' alt=""/>
+
+      </div>
+      
+    </div>
   )
 }
 
